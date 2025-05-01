2a9294d4ab2e20de31ce8267b65130ec3676ce96
JamesR-55 <jmr00046@mix.wvu.edu>
2025-05-01 17:43:09 -0400

    Done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React from 'react';
+import AppNavbar from './Navbar';
+import CardComponent from './Card';  // Import the Card component
 
 function App() {
   return (
     <div className="App">
-      <h1>CS 330L</h1>
-      <h2>Section - 11</h2>
-      <p>WVU ID: 427723</p>
-      <p>Hi I am James Radcliff</p>
+      <AppNavbar />
+      <div className="container mt-4">
+        <h1>CS 330L</h1>
+        <h2>Section - 11</h2>
+        <p>WVU ID: 427723</p>
+        <p>Hi, I am James Radcliff</p>
+
+        {/* Display the Card Component */}
+        <CardComponent />
+      </div>
     </div>
   );
 }
